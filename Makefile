9956748d02024f2d0413acc3a52bc44ed6191898
ael-malt <ael-malt@Inception.myguest.virtualbox.org>
2024-05-31 02:03:59 +0200

    IT'S ALIVE

--- Makefile
+++ Makefile
@@ -3,13 +3,14 @@ NAME = inception
 DOCKER = docker-compose -f ./srcs/docker-compose.yml
 
 all:
-	$(DOCKER) up -d --build
+	# @mkdir /home/ael-malt/data/mariadb /home/ael-malt/data/wordpress
+	$(DOCKER) up --build
 
 stop:
 	$(DOCKER) stop
 
 rm:
-	$(DOCKER) down -f
+	$(DOCKER) down
 
 # YN := $(shell bash -c 'read -p "This will prune ALL your docker images. Do you want to continue? [Y/N]:"')
 # prune:
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,42 @@
 #!/bin/sh
-service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+if [ -e "/var/lib/mysql/first_config_done" ]
+then
+	echo "Database already configured"
+else
+	echo "Creating the files change_root.sql and config_db.sql"
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_PASSWORD';" >> /var/lib/mysql/change_root.sql
+	echo "FLUSH PRIVILEGES;" >> /var/lib/mysql/change_root.sql
+	echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" > /var/lib/mysql/config_db.sql
+	echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" >> /var/lib/mysql/config_db.sql
+	echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"  >> /var/lib/mysql/config_db.sql
+	echo "FLUSH PRIVILEGES;" >> /var/lib/mysql/config_db.sql
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
+	echo "Starting MYSQL"
+	service mariadb start
+
+	echo "Configuring MYSQL"
+	mariadb -u root < /var/lib/mysql/config_db.sql
+
+	echo "Changing root password"
+	mariadb -u root < /var/lib/mysql/change_root.sql
+
+	echo "Shutting down the database"
+	mariadb-admin --user=root --password=$SQL_PASSWORD shutdown
+
+	echo "Removing temporary files"
+	rm /var/lib/mysql/change_root.sql /var/lib/mysql/config_db.sql
+
+	touch /var/lib/mysql/first_config_done
+fi
+
+echo "Launching mysql service"
 exec mysqld_safe
+
+# echo "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" > /var/lib/mysql/mariadb_init.sql
+# echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> /var/lib/mysql/mariadb_init.sql
+# echo "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> /var/lib/mysql/mariadb_init.sql
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" >> /var/lib/mysql/mariadb_init.sql
+# echo "FLUSH PRIVILEGES;" >> /var/lib/mysql/mariadb_init.sql
+
+# mysqld --init-file=/var/lib/mysql/mariadb_init.sql
